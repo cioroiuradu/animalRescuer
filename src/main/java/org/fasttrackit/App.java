f344bd4797281024d7f4f18c336b578d6c847e07
cioroiuradu <cioroiu.radu@gmail.com>
2018-12-26 16:54:22 +0200

    creare clase, definire proprietati clase. Creare instanta

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,10 +6,45 @@ package org.fasttrackit;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+    public static void main( String[] args ) {
 
-    
+    Animal firstAnimal = new Animal();
+    firstAnimal.name = "Costica";
+    firstAnimal.age = 3;
+    firstAnimal.healthLevel = 9;
+    firstAnimal.hungerLevel = 10;
+    firstAnimal.moodLevel = 7;
+    firstAnimal.favouriteFood = "Steak";
+    firstAnimal.hobby = "Ball chasing";
+
+    Adopter firstAdopter = new Adopter();
+    firstAdopter.name = "George";
+    firstAdopter.moneyAmount = 500;
+
+    Food firstFood = new Food();
+    firstFood.name = "Steak";
+    firstFood.quantity = 3;
+//    firstFood.Date = "21/12/2018";
+    firstFood.availability = true;
+
+
+    Hobby firstHobby = new Hobby();
+    firstHobby.name = "Ball Chasing";
+
+    Vet firstVet = new Vet();
+    firstVet.name = "Ion";
+    firstVet.studyField = "Surgery";
+
+    Game firstGame = new Game();
+    firstAdopter.name = "Radu";
+    firstAnimal.name = "Cat";
+    firstVet.name = "Antonescu";
+//
+//    Car firstCompetitor= new Car();
+//    firstCompetitor.name = "Dacia";
+//    firstCompetitor.color = "black";
+//    firstCompetitor.doorCount = 5;
+//    firstCompetitor.mileage = 9.5;
+//    firstCompetitor.running = false;
+    }
 }
